a6ee03cf75c372149178869cc03e4ec9c755c819
Asraful Alam <asraful.alam@mobioapp.com>
2020-12-18 20:13:41 +0600

    Initial Commit

---
+++ MultipleTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleTarget
+//
+//  Created by Steve JobsOne on 12/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
