d007c185e849bce87bfa6699f2b236bcaecebb60
Asraful Alam <asraful.alam@mobioapp.com>
2020-12-27 21:16:36 +0600

    multiple Targets

--- MultipleTarget/ViewController.swift
+++ MultipleTarget/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        #if Pro
+        print("Pro")
+        #elseif QA
+        print("Qa")
+        #else
+        print("dev")
+        
+        #endif
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
